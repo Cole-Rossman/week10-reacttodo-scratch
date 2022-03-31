652c74a055dad914308b834e4d3a00e615a86f0a
Cole Rossman <colerossman99@gmail.com>
2022-03-31 16:03:09 -0700

    todolist progress

--- src/views/TodoList.js
+++ src/views/TodoList.js
@@ -3,8 +3,23 @@ import { fetchTodos } from '../services/todos';
 
 export default function TodoList() {
   const [todos, setTodos] = useState([]);
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const todosData = await fetchTodos();
+        setTodos(todosData);
+        setLoading(false);
+      } catch (e) {
+        setError('An error has occured, please try again.');
+      }
+    };
+    fetchData();
+  }, []);
 
+  if (loading) return <h1>Loading...</h1>;
 
   return (
     <div>todoList</div>
