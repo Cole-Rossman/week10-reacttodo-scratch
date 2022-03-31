782852ed6203b39bbe0fb8cf31418c7a2b341d82
Cole Rossman <colerossman99@gmail.com>
2022-03-31 16:25:40 -0700

    createTodo added to todos.js

--- src/services/todos.js
+++ src/services/todos.js
@@ -4,3 +4,8 @@ export async function fetchTodos() {
   const resp = await client.from('todos').select('*');
   return checkError(resp);
 }
+
+export async function createTodo(todo) {
+  const resp = await client.from('todos').insert(todo);
+  return checkError(resp);
+}
