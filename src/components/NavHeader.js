2653a166c063276c76ea4c9f9eaca1954c4a0d33
Cole Rossman <colerossman99@gmail.com>
2022-03-31 15:29:09 -0700

    navHeader added with logout button

--- src/components/NavHeader.js
+++ src/components/NavHeader.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { logout } from '../services/users';
 
-export default function NavHeader() {
+export default function NavHeader({ currentUser, setCurrentUser }) {
+  const handleLogout = async () => {
+    await logout();
+    setCurrentUser(null);
+  };
+
   return (
-    <div>NavHeader</div>
+    <div className='nav-header'>
+      {currentUser && (
+        <ul>
+          <li>
+            <p>User: {currentUser}</p>
+          </li>
+          <button className='logout' onClick={handleLogout}>Logout</button>
+        </ul>
+      )}
+    </div>
   );
 }
